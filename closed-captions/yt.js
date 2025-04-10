f3db7fdb2ae4b85e5999475b58a20a385f08befd
Matthijs <stokrom@hva.nl>
2025-04-10 11:42:13 +0200

    Save

--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -4,7 +4,7 @@ function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video-placeholder', {
 		width: 600,
 		height: 400,
-		videoId: 'vrP-_T-h9YM',
+		videoId: '1vzhidDoDU0',
 		playerVars: {
 			color: 'white',
 			//start: 
